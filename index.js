8d3f2b106dab63a9710f64de0ee219dfa28ce4e9
ZHANGXIANGLIANG <326277403@qq.com>
2018-04-06 23:13:46 +0800

    init graphql http server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import express from "express"
+import express from 'express'
+import ghttp from 'express-graphql'
+import schema from './schema'
 
+const graphiql = true;
 const app = express()
 
+app.use('/graphql', ghttp({ schema, graphiql }))
 app.listen(8080)
